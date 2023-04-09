81cbdd832378a421f40a2de0f165bc27a23686ee
juanv <juanv@DESKTOP-TT7GUJN>
2023-04-09 19:25:47 -0400

    Eighth commit

---
+++ src/d2hwp4/DAY2HOMEWORKPROJECT4.java
@@ -0,0 +1,11 @@
+package d2hwp4;
+
+public class DAY2HOMEWORKPROJECT4 {
+	public static void main(String[] args) {
+		System.out.println("create the fourth local repository");
+	}
+	public void print(String a ) {
+		a = "stash this";
+		System.out.println(a);
+	}
+}
